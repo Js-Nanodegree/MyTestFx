bf48c98e36214b8712ece9fe5c19e95a3350209c
Slava <fintechmustreets@googlemail.com>
2025-06-01 12:22:53 +0500

    feat: Add global styles and fonts for consistent UI
    This commit introduces global styles and font assets to ensure a consistent and visually appealing user interface across the application.
    
    Key changes include:
    
    - Added `src/styles/globalStyles.ts` to define global styles for text, input, and container elements, using the Inter font family.
    - Added font files for various weights and styles of the Inter font family (18pt, 24pt, and 28pt) to `assets/fonts/`, including Regular, Bold, Black, Light, Medium, Thin, Italic, BoldItalic, SemiBold, ThinItalic, BlackItalic, ExtraBold, ExtraLight, ExtraLightItalic, and MediumItalic.
    - Added SpaceMono-Regular font to `assets/fonts/`.

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet, TextStyle, ViewStyle } from 'react-native';
+
+export const globalStyles = StyleSheet.create( {
+    text: {
+        fontFamily: 'Inter-Regular',
+    } as TextStyle,
+    textMedium: {
+        fontFamily: 'Inter-Medium',
+    } as TextStyle,
+    textSemiBold: {
+        fontFamily: 'Inter-SemiBold',
+    } as TextStyle,
+    textBold: {
+        fontFamily: 'Inter-Bold',
+    } as TextStyle,
+    input: {
+        fontFamily: 'Inter-Regular',
+    } as TextStyle,
+    container: {
+        flex: 1,
+    } as ViewStyle,
+} ); 
